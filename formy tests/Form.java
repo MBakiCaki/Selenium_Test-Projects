c59c51cb0f4a47fe0b000913d0d1962d5192a196
MBakiCaki <muhammedbakicaki@gmail.com>
2023-02-25 17:58:15 +0300

    form test

--- formy tests/Form.java
+++ formy tests/Form.java
@@ -1,8 +1,14 @@
 
+import junit.framework.Assert;
 import org.openqa.selenium.By;
-        import org.openqa.selenium.Keys;
-        import org.openqa.selenium.WebDriver;
-        import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Form {
     public static void main(String[] args) {
@@ -11,25 +17,50 @@ public class Form {
 
         WebDriver driver = new ChromeDriver();
 
+        // navigate to page
         driver.get("https://formy-project.herokuapp.com/form");
 
-        driver.findElement(By.id("first-name")).sendKeys("John");
+        // fill out form
+        fillForm(driver);
 
-        driver.findElement(By.id("last-name")).sendKeys("Doe");
+        // submit
+        driver.findElement(By.cssSelector("a[class='btn btn-lg btn-primary']")).click();
 
-        driver.findElement(By.id("job-title")).sendKeys("QA Engineer");
-
-        driver.findElement(By.id("radio-button-2")).click();
-
-        driver.findElement(By.id("checkbox-2")).click();
-
-        driver.findElement(By.cssSelector("option[value='1']")).click();
-
-        driver.findElement(By.id("datepicker")).sendKeys("05/28/2019");
-        driver.findElement(By.id("datepicker")).sendKeys(Keys.RETURN);
-
-        driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary")).click();
+        // wait 3 second
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        // assert alert message is successful
+        WebElement element = driver.findElement(By.className("alert"));
+        String alertMessage = element.getText();
+        Assert.assertEquals(alertMessage, "The form was successfully submitted!");
 
         driver.quit();
+    }
+
+    public static void fillForm(WebDriver driver) {
+        // fill name
+        driver.findElement(By.id("first-name")).sendKeys("Bruce");
+        driver.findElement(By.id("last-name")).sendKeys("Lee");
+
+        // fill job title
+        driver.findElement(By.id("job-title")).sendKeys("Fighter");
+
+        // select education level
+        driver.findElement(By.id("radio-button-2")).click();
+
+        // select gender
+        driver.findElement(By.id("checkbox-1")).click();
+
+        // select experince
+        driver.findElement(By.id("select-menu")).click();
+        driver.findElement(By.cssSelector("option[value='4']")).click();
+
+        // select date
+        WebElement datepicker = driver.findElement(By.id("datepicker"));
+        datepicker.sendKeys("01/01/2025");
+        datepicker.sendKeys(Keys.RETURN);
     }
 }
